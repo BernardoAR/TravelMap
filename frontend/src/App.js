d759a1cf0c1dfad2ae9d656331d7176628a35cae
Bernardo Alves Roballo <bernardo_roballo@hotmail.com>
2021-07-02 21:35:53 -0300

    Created possibilit of save new pins on the database.
    Signed-off-by: Bernardo Alves Roballo <bernardo_roballo@hotmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ function App() {
   const currentUser = 'bernardo';
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(0);
   const [newPlace, setNewPlace] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
@@ -35,6 +38,24 @@ function App() {
     const [long, lat] = e.lngLat;
     setNewPlace({ lat, long });
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+    try {
+      const res = await axios.post('/pins', newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className='App'>
       <ReactMapGL
@@ -47,7 +68,7 @@ function App() {
       >
         {pins.map((p) => (
           <>
-            <Marker latitude={p.lat} longitude={p.long} offsetLeft={-20} offsetTop={-10}>
+            <Marker latitude={p.lat} longitude={p.long} offsetLeft={-viewport.zoom * 3.5} offsetTop={-viewport.zoom * 5}>
               <Room
                 style={{
                   fontSize: viewport.zoom * 5,
@@ -72,13 +93,7 @@ function App() {
                   <label>Review</label>
                   <p className='desc'>{p.desc}</p>
                   <label>Rating</label>
-                  <div className='stars'>
-                    <Star className='star' />
-                    <Star className='star' />
-                    <Star className='star' />
-                    <Star className='star' />
-                    <Star className='star' />
-                  </div>
+                  <div className='stars'>{Array(p.rating).fill(<Star className='star' />)}</div>
                   <label>Information</label>
                   <span className='username'>
                     Created by <b>{p.username}</b>
@@ -100,20 +115,22 @@ function App() {
             anchor='bottom'
           >
             <div>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <label>Title</label>
-                <input placeholder="Enter a title"/>
+                <input placeholder='Enter a title' onChange={(e) => setTitle(e.target.value)} />
                 <label>Review</label>
-                <textarea placeholder="Say us something about this place."/>
+                <textarea placeholder='Say us something about this place.' onChange={(e) => setDesc(e.target.value)} />
                 <label>Rating</label>
-                <select>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                  <option value='4'>4</option>
+                  <option value='5'>5</option>
                 </select>
-                <button className="submitButton" type="submit">Add Pin</button>
+                <button className='submitButton' type='submit'>
+                  Add Pin
+                </button>
               </form>
             </div>
           </Popup>
